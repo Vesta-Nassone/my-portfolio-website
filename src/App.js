468d5081db5331bbd10d75eaad93fa9eb6ce3bad
Vesta-Nassone <vestanassone@gmail.com>
2021-02-11 12:27:14 +0200

    React boilerplate code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="pattern-zigzag-lg">
+        <img src={logo} className="App-logo" alt="logo"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
