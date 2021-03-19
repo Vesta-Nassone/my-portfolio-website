e3641ac301439bd45fff9bcbecd0378c5c535ade
Vesta-Nassone <vestanassone@gmail.com>
2021-03-19 23:18:03 +0200

    Adds glass morph to the nav on small screen. Retouches the icons and adds color.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import avatar from "./avatar.jpg";
-import { makeStyles } from '@material-ui/core/styles';
 import "./Navbar.css";
 import GamesIcon from '@material-ui/icons/Games';
 import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
@@ -8,66 +6,67 @@ import InfoIcon from '@material-ui/icons/Info';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import Apps from "@material-ui/icons/Apps";
 import HomeIcon from '@material-ui/icons/Home';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import List from '@material-ui/core/List';
+import { green } from '@material-ui/core/colors';
+import { red } from '@material-ui/core/colors';
+import { brown } from '@material-ui/core/colors';
+import { blue } from '@material-ui/core/colors';
+import { orange } from '@material-ui/core/colors';
+import { yellow } from '@material-ui/core/colors';
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-    listItem: {
-        color: "silver",
-        fontFamily: 'Courier Prime mono'
-    },
-}));
-
-const menuItems = [
-    { listIcon: <HomeIcon fontSize="large" />, listText: "Home", listPath: "/" },
-    { listIcon: <PersonOutlineIcon fontSize="large" />, listText: "About", listPath: "/about" },
-    { listIcon: <Apps fontSize="large" />, listText: "Projects", listPath: "/projects" },
-    { listIcon: <InfoIcon fontSize="large" />, listText: "Resume", listPath: "/resume" },
-    { listIcon: <GamesIcon fontSize="large" />, listText: "Games", listPath: "/games" },
-    { listIcon: <ContactPhoneIcon fontSize="large" />, listText: "Contact", listPath: "/contact" },
-];
 export default function Navbar() {
-    const classes = useStyles();
     return (
         <div className="w3-top">
             <nav className="sidebar w3-bar-block w3-small w3-hide-small w3-center">
-                {/*Avatar image in top left corner*/}
-                <img src={avatar} style={{ width: "100%" }} /><br /><br />
-                <List>
-                    {menuItems.map((item, i) => (
-                        <ListItem
-                            button
-                            key={i}
-                            className={classes.listItem}
-                            component={Link}
-                            to={item.listPath}
-                        >
-                            <ListItemIcon className={classes.listItem}>
-                                {item.listIcon}
-                            </ListItemIcon>
-                            <ListItemText primary={item.listText} />
-                        </ListItem>
-                    ))}
-                </List>
+                <img src={'https://drive.google.com/uc?export=view&id=1dqRHxE62MEjLCtmPG943bUqG9jCjq5Nx'} alt="Vesta Nassone" style={{ width: "100%" }} />
+                <ul className="sidebar__icons">
+                    <li>
+                        <Link to="/"><HomeIcon
+                            style={{
+                                fontSize: 50,
+                                color: green[600]
+                            }} /><p>Home</p></Link>
+                    </li>
+                    <li>
+                        <Link to="/about"><PersonOutlineIcon style={{ fontSize: 50, color: brown[800] }} /><p>About</p></Link>
+                    </li>
+                    <li>
+                        <Link to="/projects"><Apps style={{ fontSize: 50, color: blue[200] }} /><p>Projects</p></Link>
+                    </li>
+                    <li>
+                        <Link to="/resume"><InfoIcon style={{ fontSize: 50, color: red[500] }} /><p>Resume</p></Link>
+                    </li>
+                    <li>
+                        <Link to="/games"><GamesIcon style={{ fontSize: 50, color: orange[600] }} /><p>Games</p></Link>
+                    </li>
+                    <li>
+                        <Link to="/contact"><ContactPhoneIcon style={{ fontSize: 50, color: yellow[700] }} /><p>Contact</p></Link>
+                    </li>
+                </ul>
             </nav>
             {/*Navbar on small screens (Hidden on medium and large screens)*/}
-            <div className="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
-                <div className="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
-                    <a href="#" className="w3-bar-item w3-button" style={{
-                        width: "25% !important"
-                    }}>HOME</a>
-                    <a href="#about" className="w3-bar-item w3-button" style={{
-                        width: "25% !important"
-                    }}>ABOUT</a>
-                    <a href="#photos" className="w3-bar-item w3-button" style={{
-                        width: "25% !important"
-                    }}>PHOTOS</a>
-                    <a href="#contact" className="w3-bar-item w3-button" style={{
-                        width: "25% !important"
-                    }}>CONTACT</a>
+            <div className="small__sidebar w3-bottom w3-hide-large w3-hide-medium" id="myNavbar">
+                <div className="w3-bar w3-opacity w3-hover-opacity hover-black w3-center w3-small">
+                    <ul className="card__numbers">
+                        <li>
+                            <Link to="/"><HomeIcon style={{ fontSize: 50, color: green[600] }} /></Link>
+                        </li>
+                        <li>
+                            <Link to="/about"><PersonOutlineIcon style={{ fontSize: 50, color: brown[500] }} /></Link>
+                        </li>
+                        <li>
+                            <Link to="/projects"><p><Apps style={{ fontSize: 50, color: blue[600] }} /></p></Link>
+                        </li>
+                        <li>
+                            <Link to="/resume"><InfoIcon style={{ fontSize: 50, color: red[400] }} /></Link>
+                        </li>
+                        <li>
+                            <Link to="/games"><GamesIcon style={{ fontSize: 50, color: orange[600] }} /></Link>
+                        </li>
+                        <li>
+                            <Link to="/contact"><ContactPhoneIcon style={{ fontSize: 50, color: yellow[400] }} /></Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
