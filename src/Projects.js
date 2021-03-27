f0a6d2cdcd285f4c6a9561857dfaa1f0143c098f
Vesta-Nassone <vestanassone@gmail.com>
2021-03-27 18:08:29 +0200

    Creates the Project component template.

---
+++ src/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Projects.css";
+
+export default function () {
+    return (
+        <div className="projects__container">
+            <h2 className="projects__heading">Check out some of my projects</h2>
+        </div>
+    )
+}
