4172ede3239bb489eae725d3ff8e079db4013956
Vesta-Nassone <vestanassone@gmail.com>
2021-03-27 18:09:40 +0200

    Renders the templates.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./Navbar.js";
 import Home from "./Home.js";
 import Contact from "./Contact.js";
 import About from "./About.js";
+import Resume from "./Resume.js";
+import Games from "./Games.js"
+import Projects from "./Projects"
 import './App.css';
 
 function App() {
@@ -22,6 +25,9 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/about" component={About}/>
+            <Route exact path="/resume" component={Resume}/>
+            <Route exact path="/games" component={Games}/>
+            <Route exact path="/projects" component={Projects}/>
           </Switch>
         </Router>
       </div>
