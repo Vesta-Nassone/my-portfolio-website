220fb3b777eebf1d190a3720b005017170c73754
Vesta-Nassone <vestanassone@gmail.com>
2021-03-27 18:08:46 +0200

    Creates the Resume component template.

---
+++ src/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Resume.css';
+
+export default function Resume() {
+    return (
+        <div className="skills__container">
+                <h2 className="skills__heading">
+                    My Skills
+                </h2>
+        </div>
+    );
+}
