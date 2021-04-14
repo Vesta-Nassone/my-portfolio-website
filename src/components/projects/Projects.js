f870d27fe86c8d96e7cdc27a75849a4e46091d48
Vesta-Nassone <vestanassone@gmail.com>
2021-04-14 22:16:01 +0200

    Adds project templates

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -35,7 +35,7 @@ export default function Projects() {
                                     />
                                     <ImageListItemBar
                                         title={item.title}
-                                        // subtitle={item.author}
+                                        subtitle={item.author}
                                         actionIcon={
                                             <IconButton
                                                 sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
@@ -58,73 +58,40 @@ export default function Projects() {
 const itemData = [
     {
         img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-        title: 'Breakfast',
-        author: '@bkristastucchio',
+        title: 'Project 1',
+        author: '@vesta-nassone',
         rows: 2,
         cols: 2,
         featured: true,
     },
     {
         img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-        title: 'Burger',
-        author: '@rollelflex_graphy726',
+        title: 'Project 2',
+        author: '@vesta-nassone',
     },
     {
         img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-        author: '@helloimnik',
+        title: 'Project 3',
+        author: '@vesta-nassone',
     },
     {
         img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-        title: 'Coffee',
-        author: '@nolanissac',
+        title: 'Project 4',
+        author: '@vesta-nassone',
         cols: 2,
     },
     {
         img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-        title: 'Hats',
-        author: '@hjrc33',
+        title: 'Project 5',
+        author: '@vesta-nassone',
         cols: 2,
     },
     {
         img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-        title: 'Honey',
-        author: '@arwinneil',
+        title: 'Project 6',
+        author: '@vesta-nassone',
         rows: 2,
         cols: 2,
         featured: true,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-        title: 'Basketball',
-        author: '@tjdragotta',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-        title: 'Fern',
-        author: '@katie_wasserman',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
-        author: '@silverdalex',
-        rows: 2,
-        cols: 2,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-        author: '@shelleypauls',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
-        author: '@peterlaster',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
-        author: '@southside_customs',
-        cols: 2,
     },
 ];
