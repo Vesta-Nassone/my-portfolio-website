be2fb019c4ed06fcf0ce0c4d9faa3e781eead814
“Vesta <vestanassone@gmail.com>
2021-05-21 15:31:15 +0200

    Attempting to fix the form submission issue.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Contact.css";
 import IconButton from '@material-ui/core/IconButton';
 import { FaTwitter, FaGithub, FaWhatsapp, FaLinkedin } from 'react-icons/fa';
 
 
-const Contact = () => {
+function Contact() {
+  const [success, setSuccess] = useState(false);
+  useEffect(() => {
+    if (window.location.search.includes('success=true')) {
+      setSuccess(true);
+    }
+  }, []);
   return (
     <div className="contact__card">
       <h1 className="card__heading">Contact Me</h1>
+      {success && (
+        <p style={{ color: "green" }}>Thanks for your message! </p>
+      )}
       <ul className="card__numbers ">
         <li className="card__number">+27</li>
         <li className="card__number">68</li>
@@ -15,28 +24,20 @@ const Contact = () => {
         <li className="card__number">3400</li>
       </ul>
       <div>
-        <form 
+        <form
           name="contact"
-          method="POST" 
-          data-netlify="true" 
+          method="POST"
+          action="/contact/?success=true"
+          data-netlify="true"
           data-netlify-recaptcha="true"
+          netlify-honeypot="bot-field"
         >
-          <p>
-            <input placeholder="Name" autoComplete="on" className="card__input" />
-          </p>
-          <p>
-            <input placeholder="Email" autoComplete="on" className="card__input" />
-          </p>
-          <p>
-            <input placeholder="Subject" className="card__input" />
-          </p>
-          <p>
-            <input placeholder="Message" className="card__input" />
-          </p>
+          <input placeholder="Name" autoComplete="on" className="card__input" />
+          <input placeholder="Email" autoComplete="on" className="card__input" />
+          <input placeholder="Subject" className="card__input" />
+          <input placeholder="Message" className="card__input" />
           <div data-netlify-recaptcha="true"></div>
-          <p>
-            <button className="card__button">Send Message</button>
-          </p>
+          <button className="card__button" type='submit'>Send Message</button>
         </form>
       </div>
 
