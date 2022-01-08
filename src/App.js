cbd862fc40e45e0f4d528ebb79054e196c36509d
Vesta-Nassone <vestanassone@gmail.com>
2022-01-08 11:46:33 +0200

    Reformat the App.js file.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.js";
 import Home from "./components/header/Home.js";
 import Contact from "./components/contact/Contact.js";
 import Skills from "./components/skills/Skills.js";
 import Resume from "./components/resume/Resume.js";
-import Games from "./components/games/Games.js"
-import Projects from "./components/projects/Projects"
-import './App.css';
+import Games from "./components/games/Games.js";
+import Projects from "./components/projects/Projects";
+import "./App.css";
 
 function App() {
-  return (
-    <React.Fragment>
-      <div
-        className="App.css"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-        <Router>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/skills" component={Skills}/>
-            <Route exact path="/resume" component={Resume}/>
-            <Route exact path="/games" component={Games}/>
-            <Route exact path="/projects" component={Projects}/>
-          </Switch>
-        </Router>
-      </div>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<div
+				className="App.css"
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<Router>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/contact" component={Contact} />
+						<Route exact path="/skills" component={Skills} />
+						<Route exact path="/resume" component={Resume} />
+						<Route exact path="/games" component={Games} />
+						<Route exact path="/projects" component={Projects} />
+					</Switch>
+				</Router>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default App;
