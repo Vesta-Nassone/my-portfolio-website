b3cc811a4672392929ded42677620062bc2b3a18
Vesta-Nassone <vestanassone@gmail.com>
2022-01-08 17:33:28 +0200

    Rename the games folder to goals.

---
+++ src/components/goals/Goals.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Games.css"
+
+export default function Games() {
+    return (
+        <div className="games__container">
+            <h1 className="skills__heading">Play Some Games</h1>
+        </div>
+    )
+}
