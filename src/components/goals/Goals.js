18948488613cf60df4f9aeec810941e4adbcda63
Vesta-Nassone <vestanassone@gmail.com>
2022-01-08 17:57:03 +0200

    Add the glass__container class.

--- src/components/goals/Goals.js
+++ src/components/goals/Goals.js
@@ -3,7 +3,7 @@ import "./Goals.css"
 
 export default function Games() {
     return (
-        <div className="goals__container">
+        <div className="glass__container">
             <h1 className="skills__heading">2022 Goals</h1>
         </div>
     )
