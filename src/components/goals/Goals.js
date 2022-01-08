ac95bf85c2b5b1f35d305fb8c0370d4ef96d7e80
Vesta-Nassone <vestanassone@gmail.com>
2022-01-08 19:05:54 +0200

    Revert to old classname.

--- src/components/goals/Goals.js
+++ src/components/goals/Goals.js
@@ -3,7 +3,7 @@ import "./Goals.css"
 
 export default function Games() {
     return (
-        <div className="glass__container">
+        <div className="goals__container">
             <h1 className="skills__heading">2022 Goals</h1>
         </div>
     )
